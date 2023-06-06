6ed5a7ef4734a7cc843527953d12b962f0e09012
Ahmed Eid <Ahmed_tech67@outlook.com>
2023-06-06 23:22:29 +0300

    Add Functionality to the code and refactor some code

--- script.js
+++ script.js
@@ -3,34 +3,41 @@
 
 // Data
 const account1 = {
-  owner: "Jonas Schmedtmann",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
+  owner: "Ahmed Eid",
+  movements: [1050, 450, -975, 3000, -150, -1130, 4650, 1300],
+  interestRate: 0.7, // %
   pin: 1111,
 };
 
 const account2 = {
+  owner: "Jonas Schmedtmann",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 5555,
+};
+
+const account3 = {
   owner: "Jessica Davis",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
-const account3 = {
+const account4 = {
   owner: "Steven Thomas Williams",
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
-const account4 = {
+const account5 = {
   owner: "Sarah Smith",
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
 };
 
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2, account3, account4, account5];
 
 // Elements
 const labelWelcome = document.querySelector(".welcome");
@@ -59,6 +66,7 @@ const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
 // Coding
+// Display Movements List (Deposits & Withdrawals)
 const displayMovements = (movs) => {
   containerMovements.innerHTML = "";
 
@@ -74,18 +82,99 @@ const displayMovements = (movs) => {
     containerMovements.insertAdjacentHTML("afterbegin", html);
   });
 };
-displayMovements(account1.movements);
 
-const checkDogs = (dogsJulia, dogsKate) => {
-  const dogsJuliaWithoutCat = dogsJulia.slice(1, -2);
-  const allDogs = [...dogsJuliaWithoutCat, ...dogsKate];
-  allDogs.forEach((dog, i) => {
-    const check =
-      dog >= 3
-        ? `Dog number ${i + 1} is an adult, and is ${dog} years old`
-        : `Dog number ${i + 1} is still a puppy 🐶`;
-    console.log(check);
+// Display Totla Balance
+const calcDisplayBalance = (acc) => {
+  acc.balance = acc.movements.reduce((a, c) => a + c);
+  labelBalance.textContent = acc.balance + " €";
+};
+
+// Display total summary for (Deposits & Withdrawals & Interests)
+const calcDisplaySummary = (acc) => {
+  const incomes = acc.movements
+    .filter((mov) => mov > 0)
+    .reduce((a, c) => a + c);
+  labelSumIn.textContent = incomes + "€";
+
+  const outcomes = acc.movements
+    .filter((mov) => mov < 0)
+    .reduce((a, c) => a + c);
+  labelSumOut.textContent = Math.abs(outcomes) + "€";
+
+  const interest = acc.movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * acc.interestRate) / 100)
+    .filter((interest) => interest >= 1)
+    .reduce((a, c) => a + c);
+  labelSumInterest.textContent = interest.toFixed(2) + "€";
+};
+
+// Create user name for each account
+const createUserName = (accounts) => {
+  accounts.forEach((account) => {
+    account.userName = account.owner
+      .split(" ")
+      .map((e) => e[0])
+      .join("")
+      .toLowerCase();
   });
 };
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+createUserName(accounts);
+
+const updateUI = (acc) => {
+  // Display Movements
+  displayMovements(acc.movements);
+  // Display Balance
+  calcDisplayBalance(acc);
+  // Display Summary
+  calcDisplaySummary(acc);
+};
+
+// EVENTS
+// Create Login Functionality
+let currentAccount;
+
+btnLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    (account) => account.userName === inputLoginUsername.value
+  );
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and Message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(" ")[0]
+    }`;
+    containerApp.style.opacity = "1";
+
+    inputLoginUsername.value = "";
+    inputLoginPin.value = "";
+    inputLoginPin.blur();
+
+    updateUI(currentAccount);
+  }
+});
+
+// Create Transfer Money Functionality
+btnTransfer.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const amount = Number(inputTransferAmount.value);
+  const reciveAcc = accounts.find(
+    (account) => account.userName === inputTransferTo.value
+  );
+
+  if (
+    amount > 0 &&
+    reciveAcc &&
+    currentAccount.balance >= amount &&
+    inputTransferTo.value !== currentAccount.userName
+  ) {
+    reciveAcc.movements.push(amount);
+    currentAccount.movements.push(-amount);
+    updateUI(currentAccount);
+
+    inputTransferAmount.value = inputTransferTo.value = " ";
+  }
+});
